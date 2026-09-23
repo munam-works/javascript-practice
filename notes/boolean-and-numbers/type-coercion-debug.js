40dde5dc41ff2282e1937d13436c0280f2bca473
munam-works <moshabbabjamiltech@gmail.com>
2026-09-23 19:16:27 +0530

    Added buggy code with explanations for type coercion lab

---
+++ notes/boolean-and-numbers/type-coercion-debug.js
@@ -0,0 +1,23 @@
+// const firstResult = 5 + "10";
+// console.log(`5 + 10 = ${firstResult}`);
+//"10" is a string hence they both concatenate
+
+// const secondResult = "Eight" - 5;
+// console.log(`8 - 5 = ${secondResult}`);
+//"Eight" is also a string but "-" forces both side to be numbers which results in NAN (also a number)
+
+// const thirdResult = true + 5;
+// console.log(`1 + 5 = ${thirdResult}`);
+// it is correct but addition of two numbers was desired instead of the boolean
+
+// const fourthResult = false + 8;
+// console.log(`0 + 8 = ${fourthResult}`);
+// same as 3rd addition of two number was desired 
+
+// const fifthResult = "10" * 2;
+// console.log(`10 * 2 = ${fifthResult}`);
+//"10" is a string but "*" coerces it into a number so it actually is correct but desired was a number in the expression
+
+// const sixthResult = null + 22;
+// console.log(`0 + 22 = ${sixthResult}`);
+//null is zero but desired was a numeric value to get 22
